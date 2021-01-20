6d6e58d904b30cb15037420e58cab6aa25d011c3
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-01-20 20:15:54 +0100

    Tweak names for context variables

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -3,10 +3,10 @@ import React, { createContext, useState } from "react";
 export const UserContext = createContext({});
 
 const GlobalContext = ({ children }) => {
-  const [job, setJob] = useState({});
+  const [jobs, setJobs] = useState({});
 
   return (
-    <UserContext.Provider value={{ job, setJob }}>
+    <UserContext.Provider value={{ jobs, setJobs }}>
       {children}
     </UserContext.Provider>
   );
