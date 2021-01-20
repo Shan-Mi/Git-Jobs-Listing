459ac3db6f109ffbbf33ef015af14bbacc53f90c
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-01-20 20:18:14 +0100

    Fetch jobs
    1. Create function to generate correct keywords pattern for query string;
    2. Store queried jobs in context if results don't already exist

--- src/Pages/FrontPage.jsx
+++ src/Pages/FrontPage.jsx
@@ -1,17 +1,36 @@
 import React, { useContext, useRef } from "react";
+import { getJobs } from "../Api";
+import { UserContext } from "../context/GlobalContext";
 
 const FrontPage = () => {
   const descRef = useRef();
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("submitting");
+  const { jobs, setJobs } = useContext(UserContext);
+
+  const generateKeywords = (val) => {
+    const regex = /\s+/;
+    return val.trim().split(regex).join("+");
   };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const originalKeywords = descRef.current.value;
+    const generatedKeywords = generateKeywords(originalKeywords);
+
+    if (Object.keys(jobs).includes(generatedKeywords)) {
+      return jobs[generatedKeywords];
+    }
+
+    const newJobs = await getJobs(generatedKeywords);
+    setJobs({ ...jobs, [generatedKeywords]: newJobs });
+  };
+
   return (
     <div>
       <h1>Welcome to GitHub Job Listing</h1>
       <form onSubmit={handleSubmit}>
-        <input placeholder="Description" />
-        <input ref={descRef} placeholder="Write searching keywords" />
+        <label htmlFor="descption">Use SPACE to input multiple keywords</label>
+        <br />
+        <input id="descption" ref={descRef} placeholder="Job description..." />
         <button>Search</button>
       </form>
     </div>
