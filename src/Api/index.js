e076bcb9dba73c9ddab5d61bb9f1334e9d661f04
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-01-21 11:50:57 +0100

    Bug fix: for no jobs found

--- src/Api/index.js
+++ src/Api/index.js
@@ -1,9 +1,11 @@
 import axios from "axios";
+import { NO_JOBS_FOUND } from "../Constants/variables";
 
 const BASE_URL =
   "https://us-central1-wands-2017.cloudfunctions.net/githubjobs?description=";
 
 export const getJobs = async (description) => {
   const { data } = await axios.get(`${BASE_URL}${description}`);
-  return data;
+
+  return data.length === 0 ? NO_JOBS_FOUND : data;
 };
