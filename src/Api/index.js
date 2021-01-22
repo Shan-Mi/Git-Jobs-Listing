74639b1dd9f6280c776754388fba898df713e9d6
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-01-22 19:17:03 +0100

    Add try/catch block for async fun

--- src/Api/index.js
+++ src/Api/index.js
@@ -5,7 +5,10 @@ const BASE_URL =
   "https://us-central1-wands-2017.cloudfunctions.net/githubjobs?description=";
 
 export const getJobs = async (description) => {
-  const { data } = await axios.get(`${BASE_URL}${description}`);
-
-  return data.length === 0 ? NO_JOBS_FOUND : data;
+  try {
+    const { data } = await axios.get(`${BASE_URL}${description}`);
+    return data.length === 0 ? NO_JOBS_FOUND : data;
+  } catch (err) {
+    console.error(err);
+  }
 };
