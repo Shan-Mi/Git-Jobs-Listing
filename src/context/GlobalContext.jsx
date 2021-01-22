c68f197a6676dbb3b54a4fcfde86c9215a8733fc
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-01-22 21:16:29 +0100

    Auto formatting by prettier

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -3,9 +3,10 @@ import React, { createContext, useState } from "react";
 export const UserContext = createContext({});
 
 const GlobalContext = ({ children }) => {
-  const initialState = localStorage.hasOwnProperty('jobs') ? JSON.parse(localStorage.getItem('jobs')) : {}
+  const initialState = localStorage.hasOwnProperty("jobs")
+    ? JSON.parse(localStorage.getItem("jobs"))
+    : {};
   const [jobs, setJobs] = useState(initialState);
-  
 
   return (
     <UserContext.Provider value={{ jobs, setJobs }}>
