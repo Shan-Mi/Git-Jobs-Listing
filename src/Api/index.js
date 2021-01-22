170ab062a2e39ef8ec0decdbfa386e572d01475d
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-01-22 21:05:48 +0100

    Refactor: make func more generic

--- src/Api/index.js
+++ src/Api/index.js
@@ -2,11 +2,13 @@ import axios from "axios";
 import { NO_JOBS_FOUND } from "../Constants/variables";
 
 const BASE_URL =
-  "https://us-central1-wands-2017.cloudfunctions.net/githubjobs?description=";
+  "https://us-central1-wands-2017.cloudfunctions.net/githubjobs?";
 
-export const getJobs = async (description) => {
+export const fetchData = async (queryField, queryContent) => {
   try {
-    const { data } = await axios.get(`${BASE_URL}${description}`);
+    const { data } = await axios.get(
+      `${BASE_URL}${queryField}=${queryContent}`
+    );
     return data.length === 0 ? NO_JOBS_FOUND : data;
   } catch (err) {
     console.error(err);
